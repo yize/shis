d75b7397e7f9fdc7665b480a5a75359848c9c0a6
yize <yize.shc@gmail.com>
2013-03-11 22:29:35 +0800

    add some js

---
+++ src/history.js
@@ -0,0 +1,3 @@
+KISSY.add('history',function(){
+    var isSupport = window.history && window.history.pushState && window.history.replaceState && !navigator.userAgent.match(/(iPod|iPhone|iPad|WebApps\/.+CFNetwork)/);
+});
---
+++ src/url.js
@@ -0,0 +1,19 @@
+KISSY.add('url',function(){
+
+    var getUrl = function(){
+
+    };
+
+    var  parse = function(){
+
+    };
+
+    var location = function(){
+
+    };
+
+    var getUrl = function(url){
+
+    };
+
+});
